38d231851b8d6d9cecdf235e18d5c03afcb26f5c
Crowdbotics <team@crowdbotics.com>
2023-06-25 01:42:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-flower-41722",
+  url: "https://odd-flower-41722.botics.co",
+  api: "https://odd-flower-41722.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
